0cea019ab8697a921f1af19a8e7edc1278358c93
abrarhussien <abrarhussien@gmail.com>
2024-04-26 22:23:10 +0200

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Register from "./pages/Register.tsx";
+import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import { useEffect, useState } from "react";
