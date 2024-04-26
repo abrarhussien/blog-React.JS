ce6e0efc32dcc0e67a5933f4f9e43033baed34d6
abrarhussien <abrarhussien@gmail.com>
2024-04-27 00:09:45 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 import Home from "./pages/Home";
 import { useEffect, useState } from "react";
 import axios from "axios";
